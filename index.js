5136b5c87aa13f5f96ae9c7876a6444a480d5105
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-01-31 21:50:09 +0200

    s

--- index.js
+++
@@ -1,8 +0,0 @@
-// Отримуємо доступ до кнопки
-const button = document.querySelector('.menu-button');
-
-button.addEventListener('click', onClick);
-
-function onClick() {
-  console.log('Ти клікнув');
-}
